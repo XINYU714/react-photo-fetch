70b9424164c9c572179dff17fbc123ab95e3dbb1
XINYU714 <edc2312321@gmail.com>
2022-12-28 22:45:35 +0800

    fix-url

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import Homepage from './pages/Homepage';
-import About from './pages/About';
+import React from "react";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
 import { Routes, Route } from "react-router-dom";
-import './styles/style.css'
+import "./styles/style.css";
 //負責UI介面
 //在APP整個的ＵＩ架構輸出成一個oject 在傳入index.js去渲染
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
       <Routes>
-        <Route path='/' element={<Homepage />} />  
-        <Route path='/about' element={<About />} />
+        <Route
+          path="/react-photo-fetch"
+          element={<Homepage />}
+        />
+        <Route
+          path="/react-photo-fetch/about"
+          element={<About />}
+        />
       </Routes>
 
-      <Footer/>
-
+      <Footer />
     </div>
   );
 }
