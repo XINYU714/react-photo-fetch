e965ea5caaaa383f39758fcec3269b74cbe84db9
XINYU714 <edc2312321@gmail.com>
2022-12-28 23:03:32 +0800

    fix-url-2

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-### Photo Website -Fetch Data
+### Photo Website - Fetch Data
 
-網站使用 React 建立
+網站使用 React 建立，並運用 router,fetch api 等概念
 
 ### 作品連結
 
 [link](https://xinyu714.github.io/react-photo-fetch)
+
+### 說明
+
+- src/comppnent:元件 build
+- src/page:頁面
+- src/styles:scss 撰寫的樣式
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-// 這是做Nav的地方 
+import React from "react";
+import { Link } from "react-router-dom";
+// 這是做Nav的地方
 const Nav = () => {
-  return ( 
-   <nav>
-    <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
-    </ul>
-   </nav>
-  )
-}
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/react-photo-fetch">Home</Link>
+        </li>
+        <li>
+          <Link to="/react-photo-fetch/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
